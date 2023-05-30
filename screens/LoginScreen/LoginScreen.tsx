96d3ea5c2b75bbb5fcfe37ae8956fc1cf0dbd6f8
Bartu Gençcan <bartugenccandev@gmail.com>
2023-05-30 14:06:19 +0300

    Login and Register base functions created.

--- screens/LoginScreen/LoginScreen.tsx
+++ screens/LoginScreen/LoginScreen.tsx
@@ -1,32 +1,117 @@
 import React, { useState } from "react";
-import { View, TextInput, Button, Text } from "react-native";
+import { View, TextInput, Text, Alert } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Input, Button } from "@rneui/base";
+import styles from "./login.style";
+
+// Firebase
+import { FIREBASE_AUTH } from "../../firebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const LoginScreen: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [isRegistering, setIsRegistering] = useState(false);
 
   const handleLogin = () => {
-    // Giriş işlemlerini burada gerçekleştir
+    signInWithEmailAndPassword(FIREBASE_AUTH, email, password)
+      .then((userCredential) => {
+        // Login successfull
+        const user = userCredential.user;
+        console.log("Login successfull", user);
+      })
+      .catch((error) => {
+        // Login failed
+        const errorMessage = error.message;
+        Alert.alert("Login failed", errorMessage);
+      });
   };
 
   const handleRegister = () => {
-    // Kayıt işlemlerini burada gerçekleştir
+    if (password === confirmPassword) {
+      createUserWithEmailAndPassword(FIREBASE_AUTH, email, password)
+        .then(() => {
+          // Register successfull
+          Alert.alert("Register Successfull");
+          setEmail("");
+          setPassword("");
+          setConfirmPassword("");
+          setIsRegistering(!isRegistering);
+        })
+        .catch((error) => {
+          // Register failed
+          const errorMessage = error.message;
+          console.log("Register failed", errorMessage);
+
+          Alert.alert("Register failed", errorMessage);
+        });
+    } else {
+      Alert.alert("Password and Confirm Password must be the same");
+    }
   };
 
   return (
-    <View>
-      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
-      <TextInput
+    <View style={styles.container}>
+      <Input
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        leftIcon={{ type: "font-awesome", name: "envelope" }}
+      />
+      <Input
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
+        leftIcon={{ type: "font-awesome", name: "lock" }}
       />
+      {isRegistering && (
+        <Input
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry
+          leftIcon={{ type: "font-awesome", name: "lock" }}
+        />
+      )}
       {isRegistering ? (
-        <Button title="Register" onPress={handleRegister} />
+        <Button
+          title="SIGN UP"
+          buttonStyle={{
+            backgroundColor: "black",
+            borderWidth: 2,
+            borderColor: "white",
+            borderRadius: 30,
+          }}
+          containerStyle={{
+            width: 200,
+            marginHorizontal: 50,
+            marginVertical: 10,
+          }}
+          titleStyle={{ fontWeight: "bold" }}
+          onPress={handleRegister}
+        />
       ) : (
-        <Button title="Login" onPress={handleLogin} />
+        <Button
+          title="LOG IN"
+          buttonStyle={{
+            backgroundColor: "black",
+            borderWidth: 2,
+            borderColor: "white",
+            borderRadius: 30,
+          }}
+          containerStyle={{
+            width: 200,
+            marginHorizontal: 50,
+            marginVertical: 10,
+          }}
+          titleStyle={{ fontWeight: "bold" }}
+          onPress={handleLogin}
+        />
       )}
       <Text onPress={() => setIsRegistering(!isRegistering)}>
         {isRegistering
